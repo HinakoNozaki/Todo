e36bcbce5bbc21f97c185a02dcf5b7fb3364706a
HinakoNozaki <s1711462@s.tsukuba.ac.jp>
2019-09-23 15:57:29 +0900

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by 野崎陽奈子 on 2019/09/23.
+//  Copyright © 2019 野崎陽奈子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TodoTests/TodoTests.swift
@@ -0,0 +1,34 @@
+//
+//  TodoTests.swift
+//  TodoTests
+//
+//  Created by 野崎陽奈子 on 2019/09/23.
+//  Copyright © 2019 野崎陽奈子. All rights reserved.
+//
+
+import XCTest
+@testable import Todo
+
+class TodoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
